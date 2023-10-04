77d79e4ad8b6ed65f90cf3ba9e1bb01037334be8
BM-Daniel <danbabereyir@gmail.com>
2023-10-04 15:54:52 +0000

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,10 +10,12 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-  '''
-  Write a function called filter_datum that returns the log message obfuscated
-  The function should use a regex to replace occurrences of certain field values
-  '''
-  for value in fields:
-    pattern = re.compile(f'({"|".join(fields)})=[^{separator}]+')
-    return pattern.sub(fr'\1={redaction}', message)
+    '''
+    Write a function called filter_datum that returns the log message
+    obfuscated
+    The function should use a regex to replace occurrences of certain
+    field values
+    '''
+    for value in fields:
+        pattern = re.compile(f'({"|".join(fields)})=[^{separator}]+')
+        return pattern.sub(fr'\1={redaction}', message)
