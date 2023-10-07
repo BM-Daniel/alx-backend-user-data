9d1080fca1c5951d1f482164a54760b11d6727e2
BM-Daniel <danbabereyir@gmail.com>
2023-10-07 21:24:11 +0000

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,3 +30,17 @@ class BasicAuth(Auth):
                 return field_match.group('token')
 
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        '''
+        Method to decode base64 encoded value from authorization header
+        '''
+        if type(base64_authorization_header) == str:
+            try:
+                result = base64.b64decode(base64_authorization_header,
+                                          validate=True)
+
+                return result.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
