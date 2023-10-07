1e31624591f34be0f9803f96a5445dfb8fe78e88
BM-Daniel <danbabereyir@gmail.com>
2023-10-07 10:19:58 +0000

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,15 +7,19 @@ The function should use a regex to replace occurrences of certain field values
 
 import logging
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     '''
     Write a function called filter_datum that returns the log message
     obfuscated
     The function should use a regex to replace occurrences of certain
     field values
     '''
-    for value in fields:
-        pattern = re.compile(f'({"|".join(fields)})=[^{separator}]+')
-        return pattern.sub(fr'\1={redaction}', message)
+    for field in fields:
+        message = re.sub(field + '=.*?' + separator,
+                         field + '=' + redaction + separator, message)
+
+    return message
