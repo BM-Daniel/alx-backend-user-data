4b6e5cc3d5ef98b330d60855c97ab3fb577dd9b2
BM-Daniel <danbabereyir@gmail.com>
2023-10-10 17:22:17 +0000

    Task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,9 +1,11 @@
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -28,12 +30,12 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         '''
         Implement the add_user method, which has two required string arguments:
         email and hashed_password, and returns a User object. The method
-        should save the user to the database. 
+        should save the user to the database.
         '''
         try:
             new_user = User(email=email, hashed_password=hashed_password)
@@ -44,3 +46,25 @@ class DB:
             new_user = None
 
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        '''
+        Find a particular user using a filter
+        '''
+        fields, values = [], []
+
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+
+        if result is None:
+            raise NoResultFound()
+
+        return result
